6ccc0f4343143ecb3f17ac9d38005a52a65a27e6
Nishant Kabra <nishantkabra123@gmail.com>
2025-03-22 13:20:48 +0530

    added test message

--- src/main/java/com/master/Insurance/InsuranceApplication.java
+++ src/main/java/com/master/Insurance/InsuranceApplication.java
@@ -8,6 +8,9 @@ public class InsuranceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsuranceApplication.class, args);
+		System.out.println("Started master insurance application...");
+
 	}
+
 
 }
